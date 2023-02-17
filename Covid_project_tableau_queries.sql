59ceb98313101b8b3f94fa9b9d26ef1b3a13b864
AntNwook <120759249+AntNwook@users.noreply.github.com>
2023-02-17 11:51:51 -0500

    Add files via upload

---
+++ Covid_project_tableau_queries.sql
@@ -0,0 +1,59 @@
+/*
+Queries used for Tableau Project
+*/
+
+
+
+-- 1. 
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as unsigned)) as total_deaths, SUM(cast(new_deaths as unsigned))/SUM(New_Cases)*100 as DeathPercentage
+From CovidDeaths
+-- Where location = 'United States'
+where continent is not null 
+-- Group By date
+order by 1,2;
+
+
+
+-- 2. 
+
+-- Total Death by Continent
+
+Select continent, SUM(cast(new_deaths as unsigned)) as TotalDeathCount
+From CovidDeaths
+-- Where location like = 'United States'
+Where continent is not null 
+and location not in ('World', 'European Union', 'International')
+Group by Continent
+order by TotalDeathCount desc;
+
+
+-- 3.
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDeaths
+-- Where location = 'United States'
+Group by Location, Population
+order by PercentPopulationInfected desc;
+
+
+-- 4.
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDeaths
+-- Where location = 'United States'
+Group by Location, Population, date
+order by PercentPopulationInfected desc;
+
+
+
+
+
+
+
+
+
+
+
+
